15a57db3b8ab59fbf35c8082be29478f08ed4ad9
Ilya Stepanov <dev@ilyastepanov.com>
2017-07-07 02:24:46 -0700

    Generators.

--- model.py
+++ model.py
@@ -8,12 +8,49 @@
 import argparse
 import os
 import csv
+import random
 import numpy as np
 import cv2
+import sklearn.utils
+import sklearn.model_selection
 from keras import backend as K
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Dropout, Cropping2D
 from keras.optimizers import Adam
+
+
+LEARNING_RATE = 0.00005
+BATCH_SIZE = 64
+EPOCHS = 20
+
+IMAGES_AND_ANGLES = [
+    (0, 0.0),
+    (1, 0.1),
+    (2, -0.1)
+]
+
+
+def generator(samples, batch_size, base_train_folder, training=False):
+    num_samples = len(samples)
+    while True: # Loop forever so the generator never terminates
+        sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                for batch_sample_index, angle_adjust in IMAGES_AND_ANGLES:
+                    image_file = os.path.join(base_train_folder, batch_sample[batch_sample_index])
+                    image = cv2.imread(image_file)
+                    angle = float(batch_sample[3]) + angle_adjust
+                    if training and random.random() > 0.5:
+                        image = cv2.flip(image, 1)
+                        angle *= -1
+                    images.append(image)
+                    angles.append(angle)
+            x = np.array(images)
+            y = np.array(angles)
+            yield sklearn.utils.shuffle(x, y)
 
 
 def main():
@@ -36,33 +73,13 @@ def main():
 
     base_train_folder = os.path.dirname(args.driving_log)
 
-    images = []
-    angles = []
+    samples = []
     with open(args.driving_log) as csv_file:
         reader = csv.reader(csv_file)
         for line in reader:
-            center_image_file = os.path.join(base_train_folder, line[0])
-            center_image = cv2.imread(center_image_file)
-            angle = float(line[3])
-            images.append(center_image)
-            angles.append(angle)
+            samples.append(line)
 
-            left_image_file = os.path.join(base_train_folder, line[1])
-            left_image = cv2.imread(left_image_file)
-            images.append(left_image)
-            angles.append(angle + 0.1)
-
-            right_image_file = os.path.join(base_train_folder, line[2])
-            right_image = cv2.imread(right_image_file)
-            images.append(right_image)
-            angles.append(angle - 0.1)
-
-            mirrored_image = cv2.flip(center_image, 1)
-            images.append(mirrored_image)
-            angles.append(-1.0 * angle)
-
-    x_train = np.array(images)
-    y_train = np.array(angles)
+    train_samples, validation_samples = sklearn.model_selection.train_test_split(samples, test_size=0.2)
 
     input_shape = (160, 320, 3)
 
@@ -70,26 +87,34 @@ def main():
     model = Sequential()
     model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=input_shape))
     model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-    model.add(Conv2D(24, 5, 5,border_mode='valid', activation='elu', subsample=(2, 2)))
-    model.add(Conv2D(36, 5, 5,border_mode='valid', activation='elu', subsample=(2, 2)))
-    model.add(Conv2D(48, 5, 5,border_mode='valid', activation='elu', subsample=(2, 2)))
-    model.add(Conv2D(64, 3, 3,border_mode='valid', activation='elu', subsample=(1, 1)))
-    model.add(Conv2D(64, 3, 3,border_mode='valid', activation='elu', subsample=(1, 1)))
+    model.add(Conv2D(24, 5, 5, border_mode='valid', activation='elu', subsample=(2, 2), init='glorot_uniform'))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(36, 5, 5,border_mode='valid', activation='elu', subsample=(2, 2), init='glorot_uniform'))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(48, 5, 5,border_mode='valid', activation='elu', subsample=(2, 2), init='glorot_uniform'))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(64, 3, 3,border_mode='valid', activation='elu', subsample=(1, 1), init='glorot_uniform'))
+    model.add(Dropout(0.2))
+    model.add(Conv2D(64, 3, 3,border_mode='valid', activation='elu', subsample=(1, 1), init='glorot_uniform'))
+    model.add(Dropout(0.2))
     model.add(Flatten())
-    model.add(Dropout(0.4))
-    model.add(Dense(1164, activation='elu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(100, activation='elu'))
-    model.add(Dropout(0.8))
-    model.add(Dense(50, activation='elu'))
-    #model.add(Dropout(0.5))
-    model.add(Dense(10, activation='elu'))
-    #model.add(Dropout(0.5))
-    model.add(Dense(1, activation='tanh'))
+    model.add(Dense(1164, activation='elu', init='glorot_uniform'))
+    model.add(Dropout(0.2))
+    model.add(Dense(100, activation='elu', init='glorot_uniform'))
+    model.add(Dropout(0.2))
+    model.add(Dense(50, activation='elu', init='glorot_uniform'))
+    model.add(Dropout(0.2))
+    model.add(Dense(10, activation='elu', init='glorot_uniform'))
+    model.add(Dense(1, activation='tanh', init='glorot_uniform'))
 
-    model.compile(loss='mse', optimizer=Adam(lr=0.0001))
-
-    model.fit(x_train, y_train, validation_split=0.2, shuffle=True, batch_size=128, nb_epoch=15)
+    model.compile(loss='mse', optimizer=Adam(lr=LEARNING_RATE))
+    model.fit_generator(
+        generator=generator(samples=train_samples, batch_size=BATCH_SIZE, base_train_folder=base_train_folder, training=True),
+        samples_per_epoch=len(train_samples),
+        validation_data=generator(samples=validation_samples, batch_size=BATCH_SIZE, base_train_folder=base_train_folder),
+        nb_val_samples=len(validation_samples),
+        nb_epoch=EPOCHS,
+    )
     model.save(args.model_file)
 
     # workaround for https://github.com/tensorflow/tensorflow/issues/3388
