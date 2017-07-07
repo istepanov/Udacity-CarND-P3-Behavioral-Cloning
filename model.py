bf807dec06109d4f3c801033ea684f1e6eb8df4f
Ilya Stepanov <dev@ilyastepanov.com>
2017-07-06 22:27:55 -0700

    NVIDIA CNN

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ import csv
 import numpy as np
 import cv2
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Dropout
+from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Dropout, BatchNormalization
 
 
 def main():
@@ -41,27 +41,36 @@ def main():
         for line in reader:
             image_file = os.path.join(base_train_folder, line[0])
             image = cv2.imread(image_file)
-            image = cv2.resize(image, (32, 32))
             images.append(image)
             angles.append(float(line[3]))
 
     x_train = np.array(images)
     y_train = np.array(angles)
 
-    # LeNet
-    model = Sequential()
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(32, 32, 3)))
-    model.add(Conv2D(32, 3, 3, activation='relu', input_shape=(32, 32, 3)))
-    model.add(Conv2D(64, 3, 3, activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1))
+    input_shape = (160, 320, 3)
 
+    # nVidia
+    model = Sequential()
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=input_shape))
+    # model.add(BatchNormalization(epsilon=0.001, mode=2, axis=1, input_shape=input_shape))
+    model.add(Conv2D(24,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
+    model.add(Conv2D(36,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
+    model.add(Conv2D(48,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
+    model.add(Conv2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
+    model.add(Conv2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
+    #model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(1164, activation='relu'))
+    #model.add(Dropout(0.5))
+    model.add(Dense(100, activation='relu'))
+    #model.add(Dropout(0.5))
+    model.add(Dense(50, activation='relu'))
+    #model.add(Dropout(0.5))
+    model.add(Dense(10, activation='relu'))
+    #model.add(Dropout(0.5))
+    model.add(Dense(1, activation='tanh'))
     model.compile(loss='mse', optimizer='adam')
-    model.fit(x_train, y_train, validation_split=0.2, shuffle=True, batch_size=128, nb_epoch=10)
+    model.fit(x_train, y_train, validation_split=0.2, shuffle=True, batch_size=128, nb_epoch=15)
 
     model.save(args.model_file)
 
