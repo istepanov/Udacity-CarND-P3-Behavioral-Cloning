64fd6125f534082213a3ce09b4c7b2213b82c49f
Ilya Stepanov <dev@ilyastepanov.com>
2017-07-07 16:21:29 -0700

    Remove activation function from the last layer + early stopping.

--- model.py
+++ model.py
@@ -17,21 +17,23 @@ from keras import backend as K
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Dropout, Cropping2D
 from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
 
 
 LEARNING_RATE = 0.00005
 BATCH_SIZE = 64
 EPOCHS = 20
 
-IMAGES_AND_ANGLES = [
-    (0, 0.0),
-    (1, 0.1),
-    (2, -0.1)
-]
-
 
 def generator(samples, batch_size, base_train_folder, training=False):
     num_samples = len(samples)
+
+    images_and_angles = [
+        (0, 0.0),   # center
+        (1, 0.1),   # left
+        (2, -0.1),  # right
+    ] if training else [(0, 0.0)] # just center if not training
+
     while True: # Loop forever so the generator never terminates
         sklearn.utils.shuffle(samples)
         for offset in range(0, num_samples, batch_size):
@@ -39,9 +41,10 @@ def generator(samples, batch_size, base_train_folder, training=False):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                for batch_sample_index, angle_adjust in IMAGES_AND_ANGLES:
+                for batch_sample_index, angle_adjust in images_and_angles:
                     image_file = os.path.join(base_train_folder, batch_sample[batch_sample_index])
                     image = cv2.imread(image_file)
+                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                     angle = float(batch_sample[3]) + angle_adjust
                     if training and random.random() > 0.5:
                         image = cv2.flip(image, 1)
@@ -89,13 +92,13 @@ def main():
     model.add(Lambda(lambda x: (x / 255.0) - 0.5))
     model.add(Conv2D(24, 5, 5, border_mode='valid', activation='elu', subsample=(2, 2), init='glorot_uniform'))
     model.add(Dropout(0.2))
-    model.add(Conv2D(36, 5, 5,border_mode='valid', activation='elu', subsample=(2, 2), init='glorot_uniform'))
+    model.add(Conv2D(36, 5, 5, border_mode='valid', activation='elu', subsample=(2, 2), init='glorot_uniform'))
     model.add(Dropout(0.2))
-    model.add(Conv2D(48, 5, 5,border_mode='valid', activation='elu', subsample=(2, 2), init='glorot_uniform'))
+    model.add(Conv2D(48, 5, 5, border_mode='valid', activation='elu', subsample=(2, 2), init='glorot_uniform'))
     model.add(Dropout(0.2))
-    model.add(Conv2D(64, 3, 3,border_mode='valid', activation='elu', subsample=(1, 1), init='glorot_uniform'))
+    model.add(Conv2D(64, 3, 3, border_mode='valid', activation='elu', subsample=(1, 1), init='glorot_uniform'))
     model.add(Dropout(0.2))
-    model.add(Conv2D(64, 3, 3,border_mode='valid', activation='elu', subsample=(1, 1), init='glorot_uniform'))
+    model.add(Conv2D(64, 3, 3, border_mode='valid', activation='elu', subsample=(1, 1), init='glorot_uniform'))
     model.add(Dropout(0.2))
     model.add(Flatten())
     model.add(Dense(1164, activation='elu', init='glorot_uniform'))
@@ -105,15 +108,21 @@ def main():
     model.add(Dense(50, activation='elu', init='glorot_uniform'))
     model.add(Dropout(0.2))
     model.add(Dense(10, activation='elu', init='glorot_uniform'))
-    model.add(Dense(1, activation='tanh', init='glorot_uniform'))
+    model.add(Dense(1, init='glorot_uniform'))
 
     model.compile(loss='mse', optimizer=Adam(lr=LEARNING_RATE))
+
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='min')
+    training_generator = generator(samples=train_samples, batch_size=BATCH_SIZE, base_train_folder=base_train_folder, training=True)
+    validation_generator = generator(samples=validation_samples, batch_size=BATCH_SIZE, base_train_folder=base_train_folder)
+
     model.fit_generator(
-        generator=generator(samples=train_samples, batch_size=BATCH_SIZE, base_train_folder=base_train_folder, training=True),
-        samples_per_epoch=len(train_samples),
-        validation_data=generator(samples=validation_samples, batch_size=BATCH_SIZE, base_train_folder=base_train_folder),
+        generator=training_generator,
+        samples_per_epoch=len(train_samples) * 3,   # 3 images per sample
+        validation_data=validation_generator,
         nb_val_samples=len(validation_samples),
         nb_epoch=EPOCHS,
+        callbacks=[early_stopping]
     )
     model.save(args.model_file)
 
