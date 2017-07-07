77dad2779e5694861ccd767d9dcb03c241dd17a4
Ilya Stepanov <dev@ilyastepanov.com>
2017-07-07 00:53:04 -0700

    Tuning up the model.

--- model.py
+++ model.py
@@ -10,8 +10,10 @@ import os
 import csv
 import numpy as np
 import cv2
+from keras import backend as K
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Dropout, BatchNormalization
+from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Dropout, Cropping2D
+from keras.optimizers import Adam
 
 
 def main():
@@ -39,40 +41,59 @@ def main():
     with open(args.driving_log) as csv_file:
         reader = csv.reader(csv_file)
         for line in reader:
-            image_file = os.path.join(base_train_folder, line[0])
-            image = cv2.imread(image_file)
-            images.append(image)
-            angles.append(float(line[3]))
+            center_image_file = os.path.join(base_train_folder, line[0])
+            center_image = cv2.imread(center_image_file)
+            angle = float(line[3])
+            images.append(center_image)
+            angles.append(angle)
+
+            left_image_file = os.path.join(base_train_folder, line[1])
+            left_image = cv2.imread(left_image_file)
+            images.append(left_image)
+            angles.append(angle + 0.1)
+
+            right_image_file = os.path.join(base_train_folder, line[2])
+            right_image = cv2.imread(right_image_file)
+            images.append(right_image)
+            angles.append(angle - 0.1)
+
+            mirrored_image = cv2.flip(center_image, 1)
+            images.append(mirrored_image)
+            angles.append(-1.0 * angle)
 
     x_train = np.array(images)
     y_train = np.array(angles)
 
     input_shape = (160, 320, 3)
 
-    # nVidia
+    # NVIDIA
     model = Sequential()
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=input_shape))
-    # model.add(BatchNormalization(epsilon=0.001, mode=2, axis=1, input_shape=input_shape))
-    model.add(Conv2D(24,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
-    model.add(Conv2D(36,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
-    model.add(Conv2D(48,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
-    model.add(Conv2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
-    model.add(Conv2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
-    #model.add(Dropout(0.25))
+    model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=input_shape))
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+    model.add(Conv2D(24, 5, 5,border_mode='valid', activation='elu', subsample=(2, 2)))
+    model.add(Conv2D(36, 5, 5,border_mode='valid', activation='elu', subsample=(2, 2)))
+    model.add(Conv2D(48, 5, 5,border_mode='valid', activation='elu', subsample=(2, 2)))
+    model.add(Conv2D(64, 3, 3,border_mode='valid', activation='elu', subsample=(1, 1)))
+    model.add(Conv2D(64, 3, 3,border_mode='valid', activation='elu', subsample=(1, 1)))
     model.add(Flatten())
-    model.add(Dense(1164, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(1164, activation='elu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(100, activation='elu'))
+    model.add(Dropout(0.8))
+    model.add(Dense(50, activation='elu'))
     #model.add(Dropout(0.5))
-    model.add(Dense(100, activation='relu'))
-    #model.add(Dropout(0.5))
-    model.add(Dense(50, activation='relu'))
-    #model.add(Dropout(0.5))
-    model.add(Dense(10, activation='relu'))
+    model.add(Dense(10, activation='elu'))
     #model.add(Dropout(0.5))
     model.add(Dense(1, activation='tanh'))
-    model.compile(loss='mse', optimizer='adam')
-    model.fit(x_train, y_train, validation_split=0.2, shuffle=True, batch_size=128, nb_epoch=15)
 
+    model.compile(loss='mse', optimizer=Adam(lr=0.0001))
+
+    model.fit(x_train, y_train, validation_split=0.2, shuffle=True, batch_size=128, nb_epoch=15)
     model.save(args.model_file)
+
+    # workaround for https://github.com/tensorflow/tensorflow/issues/3388
+    K.clear_session()
 
 
 if __name__ == '__main__':
